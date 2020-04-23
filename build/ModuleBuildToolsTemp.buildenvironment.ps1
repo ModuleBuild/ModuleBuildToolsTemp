f8dca7ad711b76dcde5c15ca3d0bf0207b305b5a
Justin-p <justin-p@users.noreply.github.com>
2020-04-23 14:00:43 +0200

    version bump

--- build/ModuleBuildToolsTemp.buildenvironment.ps1
+++ build/ModuleBuildToolsTemp.buildenvironment.ps1
@@ -16,7 +16,7 @@ if ((Get-Variable 'BuildEnv' -ErrorAction:SilentlyContinue) -eq $null) {
         ForceInstallModule = $False
         Encoding = 'utf8'
         ModuleToBuild = 'ModuleBuildToolsTemp'
-        ModuleVersion = '0.0.1'
+        ModuleVersion = '0.0.4'
         ModuleWebsite = 'https://github.com/justin-p/ModuleBuildToolsTemp'
         ModuleCopyright = "(c) $((get-date).Year.ToString()) Justin Perdok. All rights reserved."
         ModuleLicenseURI = 'https://github.com/justin-p/ModuleBuildToolsTemp/LICENSE.md'
